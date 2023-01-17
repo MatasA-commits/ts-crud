b8ef955393eca9d199f31ba02447a7beb0e0d453
MatasA <aleliunasmatas@gmail.com>
2023-01-17 16:16:09 +0200

    task 4 finished: class App created

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,5 +1,12 @@
+import CarsCollection from '../helpers/cars-collection';
+import cars from '../data/cars';
+import brands from '../data/brands';
+import models from '../data/models';
+
 class App {
   private htmlElement: HTMLElement;
+
+  private carsCollection: CarsCollection;
 
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
@@ -7,6 +14,12 @@ class App {
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
     this.htmlElement = foundElement;
+    this.carsCollection = new CarsCollection({
+      cars,
+      brands,
+      models,
+    });
+    console.log(this.carsCollection);
   }
 
   initialize = (): void => {
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,4 @@
 import App from './components/app';
-import CarsCollection from './helpers/cars-collection';
-import cars from './data/cars';
-import brands from './data/brands';
-import models from './data/models';
-
-const carsCollection = new CarsCollection({
-  cars, brands, models,
-});
-
-console.log(carsCollection.allCars);
 
 const app = new App('#root');
 app.initialize();
